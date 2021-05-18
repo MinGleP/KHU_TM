e01c8423effaf84cb045c20c3fbe3cd509b831f5
Min <noona96@naver,com>
2021-05-18 20:45:38 +0900

    Remove old code

--- TextureMapping_KHU/TextureMapping_KHU/MaterialClassifier.cpp
+++
@@ -1,3 +0,0 @@
-#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
-//#include "TextureMapper.h"
-//#include "FileManager.h"
--- TextureMapping_KHU/TextureMapping_KHU/TextureMapper.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
-#include "TextureMapper.h"
-
-int TextureMapper::initialize(std::string tmp)
-{
-
-
-	return 1;
-}
